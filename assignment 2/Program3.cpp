5ed619c25792aea0c3d408ab27a19c520e81e7fe
chanhwi <chanhwi@unist.ac.kr>
2020-05-03 15:59:12 +0900

    Program3 revised

--- assignment 2/Program3.cpp
+++ assignment 2/Program3.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
+	
 	srand((unsigned int)time(0));
 	rand();
 	cout << setprecision(6);
 	cout << fixed;
 	for (int i = 0; i < 3; i++) {
-		cout << rand() / 32767.0 << ' ';
+		cout << rand() % 32767 / 32767.0 << ' ';
 	}
+	cout << endl;
 	return 0;
 }
