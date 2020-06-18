e3f85b3fc90054287b63876476f50c03e494c077
chanhwi_Hwang <chanhwi@unist.ac.kr>
2020-06-18 11:33:16 +0900

    Add comments

--- Assignment 6/Program1.cpp
+++ Assignment 6/Program1.cpp
@@ -1,31 +1,33 @@
+//20191339 ChanhwiHwang
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
-void mySwap(double& num1, double& num2) {
+void mySwap(double& num1, double& num2) {  // mySwap function to swap two double variables
 	double temp = num2;
 	num2 = num1;
 	num1 = temp;
 }
-//void mySwap(string& str1, string& str2) {
-//	string temp = str2;
-//	str2 = str1;
-//	str1 = temp;
-//}
+void mySwap(string& str1, string& str2) {  // mySwap function to swap two string variables
+	string temp = str2;
+	str2 = str1;
+	str1 = temp;
+}
 
-bool is_number(string str) {
-	int dotCount = 0;
-	for (int i = 0; i < str.size(); i++) {
+bool is_number(string str) {  // Determine whether str is real-valued number
+	int dotCount = 0;  // number of dot in str
+	for (int i = 0; i < str.length(); i++) {
 		if (i == 0)
-			if (str[0] == '-' || str[0] == '+') continue;
-		if (!isdigit(str[i]) && str[i] != '.') return false;
-		if (str[i] == '.') {
-			if (i == 0 || i == str.size() - 1) return false;
+			if (str[0] == '-' || str[0] == '+') continue;  // Sign at the front is OK
+		if (!isdigit(str[i]) && str[i] != '.') return false;  // Special Characters are not allowed
+		if (str[i] == '.') {  // When the character is dot
+		    // Check whether the position of dot is correct and its front and back character is digit
+			if (i == 0 || i == str.length() - 1) return false;
 			if (!isdigit(str[i - 1])) return false;
 			dotCount++;
 		}
-		if (dotCount >= 2) return false;
+		if (dotCount >= 2) return false;  // number of dot should be 0 or 1
 	}
 	return true;
 }
@@ -34,50 +36,54 @@ int main() {
 	fstream inFile, outFile;
 	inFile.open("Program1Input.txt", ios::in);
 	outFile.open("Program1Output20191339.txt", ios::out);
-	if (inFile.fail()) {
+	if (inFile.fail()) {  // When input file does not exist -> error case #0
 		outFile << "Error#0" << '\n';
 		outFile.close();
 		return -1;
 	}
-	int index = 0, count = 0;
-//	string temp[50];
-	string temp;
+	int count = 0;  // A variable to store the number of inputs and refer to current index
+	// Define arrays to store inputs as itself(temp) and as numbers(nums)
+	string temp[50];
 	double nums[50];
 	
-	int curIndex = inFile.tellg();
-//	inFile >> temp[index];
-	inFile >> temp;
-	while (!inFile.eof()) {
-//		if (!is_number(temp[index])) {
-		if (!is_number(temp)) {
+	int curIndex = inFile.tellg();  // Store current cursor location
+	inFile >> temp[count];
+	while (!inFile.eof()) {  // Check if the current input is number
+		if (!is_number(temp[count])) {  // There's non-number string -> error case #1
 			outFile << "Error#1" << '\n';
 			inFile.close();
 			outFile.close();
 			return -1;
 		}
+		if (temp[count][0] == '+') {  // If it has + sign, eliminate the sign character since +a = a, a is real number 
+			string str;
+			for (int i = 1; i < temp[count].length(); i++) str += temp[count][i];
+			temp[count] = str;
+		}
+		// Move to stored location to read current input again as number, not string
 		inFile.clear();
 		inFile.seekg(curIndex, ios::beg);
-		inFile >> nums[index++];
-		count++;
+		inFile >> nums[count++];  // Update the number of inputs
+		// Read next input and store new cursor location
 		curIndex = inFile.tellg();
-//		inFile >> temp[index];
-		inFile >> temp;
+		inFile >> temp[count];
 	}
 	inFile.close();
 	
-	//sorting
+	//sorting - Selection Sort : repeatedly find smallest element and locate it at the front of current iteration's array(indices with i ~ end)
+	// => In i-th loop, i-th element becomes smallest among i ~ end elements -> compare i-th element with rest of elements and if i-th element is larger, swap them to keep i-th element
+	// be the smallest element 
 	for (int i = 0; i < count - 1; i ++) {
 		for (int j = i + 1; j < count; j++) {
 			if (nums[i] > nums[j]) {
 				mySwap(nums[i], nums[j]);
-//				mySwap(temp[i], temp[j]);
+				mySwap(temp[i], temp[j]);
 			}
 		}
 	}
 	
 	for (int i = 0; i < count; i++) {
-//		outFile << temp[i] << (i == count-1 ? "" : " ");
-		outFile << nums[i] << (i == count-1 ? "" : " ");
+		outFile << temp[i] << (i == count-1 ? "" : " ");  // Print sorted numbers seperated with whitespace to output file
 	}
 	
 	outFile.close();
--- Assignment 6/Program2.cpp
+++ Assignment 6/Program2.cpp
@@ -1,31 +1,33 @@
+//20191339 ChanhwiHwang
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <string>
 using namespace std;
 
-bool is_number(string str) {
-	int dotCount = 0;
-	for (int i = 0; i < str.size(); i++) {
+bool is_number(string str) {  // Determine whether str is real-valued number
+	int dotCount = 0;  // number of dot in str
+	for (int i = 0; i < str.length(); i++) {
 		if (i == 0)
-			if (str[0] == '-' || str[0] == '+') continue;
-		if (!isdigit(str[i]) && str[i] != '.') return false;
-		if (str[i] == '.') {
-			if (i == 0 || i == str.size() - 1) return false;
+			if (str[0] == '-' || str[0] == '+') continue;  // Sign at the front is OK
+		if (!isdigit(str[i]) && str[i] != '.') return false;  // Special Characters are not allowed
+		if (str[i] == '.') {  // When the character is dot
+		    // Check whether the position of dot is correct and its front and back character is digit
+			if (i == 0 || i == str.length() - 1) return false;
 			if (!isdigit(str[i - 1])) return false;
 			dotCount++;
 		}
-		if (dotCount >= 2) return false;
+		if (dotCount >= 2) return false;  // number of dot should be 0 or 1
 	}
 	return true;
 }
 
-fstream inFile1, inFile2, outFile;
-void exitWithError(int errorType) {
+fstream inFile1, inFile2, outFile;  // Filestreams
+void exitWithError(int errorType) {  // Function to handle the exceptions
 	outFile << "Error#" << errorType << '\n';
 	outFile.close();
-	if (inFile1.is_open()) inFile1.close();
-	if (inFile2.is_open()) inFile2.close();
+	inFile1.close();
+	inFile2.close();
 }
 
 int main() {
@@ -33,38 +35,43 @@ int main() {
 	inFile2.open("Program2InputB.txt", ios::in);
 	outFile.open("Program2Output20191339.txt", ios::out);
 	
-	
-	if (inFile1.fail() || inFile2.fail()) {
-		exitWithError(0);
+	if (inFile1.fail() || inFile2.fail()) {  // Any of two input files not exist -> error case #0
+		outFile << "Error#0" << '\n';
+		outFile.close();
+		if (!inFile1.fail()) inFile1.close();  // Since it is not sure that both input files are opened, check whether it is open and if so, close it
+		if (!inFile2.fail()) inFile2.close();
 		return -1;
 	}
 	
-	int rowA, colA, rowB, colB;
+	int rowA, colA, rowB, colB;  // The number of rows and columns of each matrix
 	inFile1 >> rowA >> colA;
 	inFile2 >> rowB >> colB;
-	if (colA != rowB) {
+	if (colA != rowB) {  // If the number of column of A and the number of row of B are not equal -> matrix product does not defined -> error case #2
 		exitWithError(2);
 		return -1;
 	}
 	
-	double matrixA[rowA][colA];
+	double matrixA[rowA][colA];  // 2-dimensional arrays to store matrix entries
 	double matrixB[rowB][colB];
-	string temp;
+	string temp;  // Temporary variable to use as argument to is_number to check each input value is number
 	
 	for (int i = 0; i < rowA; i++) {
 		for (int j = 0; j < colA; j++) {
-			int curIndex = inFile1.tellg();
+			int curIndex = inFile1.tellg();  // Store current cursur location
 			inFile1 >> temp;
-			if (!is_number(temp)) {
-				exitWithError(1);
+			if (!is_number(temp)) {  // Check whether current input is number
+				exitWithError(1);  // If it is non-number -> error case #1
 				return -1;
 			}
+			// Move back to the stored location and read the input value as number
+			inFile1.clear();
 			inFile1.seekg(curIndex);
 			inFile1 >> matrixA[i][j];
 		}
 	}
 	inFile1.close();
 	
+	// Same processes as above
 	for (int i = 0; i < rowB; i++) {
 		for (int j = 0; j < colB; j++) {
 			int curIndex = inFile2.tellg();
@@ -73,26 +80,27 @@ int main() {
 				exitWithError(1);
 				return -1;
 			}
+			inFile2.clear();
 			inFile2.seekg(curIndex);
 			inFile2 >> matrixB[i][j];
 		}
 	}
 	inFile2.close();
 	
-	outFile << rowA << ' ' << colB << '\n';
+	outFile << rowA << ' ' << colB << '\n';  // Write the rows and columns of output matrix
 	
-	outFile << setprecision(6);
+	outFile << setprecision(6);  // Set the precision to 6 digits after the decimal point(by rounding off)
 	outFile << fixed;
 	
 	for (int i = 0; i < rowA; i++) {
 		for (int j = 0; j < colB; j++) {
 			double sum = 0;
 			for (int k = 0; k < colA; k++) {
-				sum += matrixA[i][k] * matrixB[k][j];
+				sum += matrixA[i][k] * matrixB[k][j];  // Calculate the each entry of output matrix by finding inner product of i-th row of matrixA and j-th column of matrixB
 			}
-			outFile << sum << (j == colB - 1 ? '\n' : ' ');
+			outFile << sum << (j == colB - 1 ? '\n' : ' ');  // Write the calculated entry and seperator character, if it reaches the end of each column, insert '\n' to move to new line, otherwise, insert whitespace
 		}
-	} 
+	}
 	outFile.close();
 	return 0; 
 }
